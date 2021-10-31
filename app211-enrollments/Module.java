fc536b21018b98f4c4e271d29bb1e506ac025423
Lekanakande <91736380+Lekanakande@users.noreply.github.com>
2021-10-31 15:39:39 +0000

    Create Module.java

---
+++ app211-enrollments/Module.java
@@ -0,0 +1,27 @@
+
+/**
+ * Write a description of class Module here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Module
+{
+     private String code;
+     
+     private String title;
+     
+     private int credit;
+    
+    /**
+     * Constructor for objects of class Module
+     */
+    public Module(String code, String title)
+    {
+        this.code = code;
+        this.title = title;        // initialise instance variables
+        
+    }
+
+ 
+}
